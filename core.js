11364baf8c68682980a85a6c7e00fe9f843babe4
Orso <orsoubox@gmail.com>
2022-01-22 14:12:05 +0100

    added support wheels for database requests

--- core.js
+++ core.js
@@ -11,15 +11,24 @@ module.exports.newUser = async function (args1, args2) {
 
 module.exports.editUser = async function (args1, args2) {
     let edit_user = await User.findOne({ id: args1 });
-    edit_user.data = `${args2}`;
-    await edit_user.save();
-    console.log('User\'s data has been edited!');
+    if (!edit_user) return undefined;
+    if (edit_user) {
+        if (typeof args2 !== 'string') return undefined;
+        if (typeof args2 === 'string') {
+            edit_user.data = `${args2}`;
+            await edit_user.save();
+            console.log('User\'s data has been edited!');
+        }
+    }
 }
 
 module.exports.getUser = async function (args1) {
     let get_user = await User.findOne({id: args1});
-    console.log('User found!');
-    return get_user.data;
+    if (!get_user) return undefined;
+    if (get_user) {
+        console.log('User found!');
+        return get_user.data;
+    }
 }
 
 module.exports.removeUser = async function (args1) {
@@ -35,15 +44,24 @@ module.exports.newGuild = async function (args1, args2) {
 
 module.exports.editGuild = async function (args1, args2) {
     let edit_guild = await Guild.findOne({ id: args1 });
-    edit_guild.data = `${args2}`;
-    await edit_guild.save();
-    console.log('Guild\'s data has been edited!');
+    if (!edit_guild) return undefined;
+    if (edit_guild) {
+        if (typeof args2 !== 'string') return undefined;
+        if (typeof args2 === 'string') {
+            edit_guild.data = `${args2}`;
+            await edit_guild.save();
+            console.log('Guild\'s data has been edited!');
+        }
+    }
 }
 
 module.exports.getGuild = async function (args1) {
     let get_guild = await Guild.findOne({id: args1});
-    console.log('Guild found!');
-    return get_guild.data;
+    if (!get_guild) return undefined;
+    if (get_guild) {
+        console.log('Guild found!');
+        return get_guild.data;
+    }
 }
 
 module.exports.removeGuild = async function (args1) {
